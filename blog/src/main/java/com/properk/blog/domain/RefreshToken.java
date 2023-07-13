c4cd924e28cd7fc660dbcca19b7f139f5cf918bf
Jeongmo <wjdah1402@gmail.com>
2023-07-13 15:15:07 +0900

    :art: Make a RefreshToken and TokenAuthenticationFilter

---
+++ blog/src/main/java/com/properk/blog/domain/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.properk.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Getter
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String refreshToken) {
+        this.refreshToken = refreshToken;
+        return this;
+    }
+}
