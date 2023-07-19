7f0d0031c5fbb707e06f2f9416d337b4b832c2a1
Jeongmo <wjdah1402@gmail.com>
2023-07-19 16:02:17 +0900

    :art: Create New Config in order to use OAuth2, JWT Token

---
+++ blog/src/main/java/com/properk/blog/config/WebOAuthSecurityConfig.java
@@ -0,0 +1,100 @@
+package com.properk.blog.config;
+
+import com.properk.blog.config.jwt.TokenProvider;
+import com.properk.blog.config.oauth.OAuth2UserCustomService;
+import com.properk.blog.repository.RefreshTokenRepository;
+import com.properk.blog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebOAuthSecurityConfig {
+
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/static/**"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .httpBasic(HttpBasicConfigurer<HttpSecurity>::disable)
+                .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
+                .logout(LogoutConfigurer<HttpSecurity>::disable);
+
+        http.sessionManagement( session -> session.
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests(author -> author
+                .requestMatchers("/api/token").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll());
+
+        http.oauth2Login(login -> login
+                .loginPage("/login")
+                .authorizationEndpoint()
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                        .successHandler(oAuth2AuthorizationSuccessHandler())
+                        .userInfoEndpoint()
+                        .userService(oAuth2UserCustomService)
+                );
+
+        http.logout(login -> login.logoutSuccessUrl("/login"));
+
+        http.exceptionHandling(exception -> exception
+                .defaultAuthenticationEntryPointFor(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                        new AntPathRequestMatcher("/api/**")
+                ));
+
+        return http.build();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OAuth2AuthorizationSuccessHandler oAuth2AuthorizationSuccessHandler() {
+        return new OAuth2AuthorizationSuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository());
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationREquestBaseOnCookieRepository(); // TODO: implement it.
+    }
+
+}
--- blog/src/main/java/com/properk/blog/config/WebSecurityConfig.java
+++ blog/src/main/java/com/properk/blog/config/WebSecurityConfig.java
@@ -1,67 +1,68 @@
-package com.properk.blog.config;
-
-import com.properk.blog.service.UserDetailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
-@RequiredArgsConstructor
-@Configuration
-public class WebSecurityConfig {
-
-    private final UserDetailService userService;
-    private static final String LOGIN = "/login";
-
-    // Deactivate spring security
-    @Bean
-    public WebSecurityCustomizer configure() {
-        return (web -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers("/static/**"));
-    }
-
-    // Configure web-based security for specific http requests
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authentication -> authentication
-                    .requestMatchers(LOGIN).permitAll()
-                    .requestMatchers("/signup").permitAll()
-                    .requestMatchers("/user").permitAll()
-                    .anyRequest().authenticated())
-                .formLogin(login -> login
-                    .loginPage(LOGIN)
-                    .defaultSuccessUrl("/articles"))
-                .logout(logout -> logout
-                    .logoutSuccessUrl(LOGIN)
-                    .invalidateHttpSession(true))
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder
-            , UserDetailService userDetailService) throws Exception {
-
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        authenticationManagerBuilder
-                .userDetailsService(userService)
-                .passwordEncoder(bCryptPasswordEncoder);
-
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package com.properk.blog.config;
+//
+//import com.properk.blog.service.UserDetailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebSecurityConfig {
+//
+//    private final UserDetailService userService;
+//    private static final String LOGIN = "/login";
+//
+//    // Deactivate spring security
+//    @Bean
+//    public WebSecurityCustomizer configure() {
+//        return (web -> web.ignoring()
+//                .requestMatchers(toH2Console())
+//                .requestMatchers("/static/**"));
+//    }
+//
+//    // Configure web-based security for specific http requests
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(authentication -> authentication
+//                    .requestMatchers(LOGIN).permitAll()
+//                    .requestMatchers("/signup").permitAll()
+//                    .requestMatchers("/user").permitAll()
+//                    .anyRequest().authenticated())
+//                .formLogin(login -> login
+//                    .loginPage(LOGIN)
+//                    .defaultSuccessUrl("/articles"))
+//                .logout(logout -> logout
+//                    .logoutSuccessUrl(LOGIN)
+//                    .invalidateHttpSession(true))
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder
+//            , UserDetailService userDetailService) throws Exception {
+//
+//        AuthenticationManagerBuilder authenticationManagerBuilder =
+//                http.getSharedObject(AuthenticationManagerBuilder.class);
+//
+//        authenticationManagerBuilder
+//                .userDetailsService(userService)
+//                .passwordEncoder(bCryptPasswordEncoder);
+//
+//        return authenticationManagerBuilder.build();
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
+// Convert it to WebOAuthSecurityConfig.java
