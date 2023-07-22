62b5a0d137b94654e6f5ed78c184b21a4b4bec74
Jeongmo <wjdah1402@gmail.com>
2023-07-22 13:56:36 +0900

    :zap: Create img for OAuth2, and modify js and html

--- blog/src/main/resources/static/js/article.js
+++ blog/src/main/resources/static/js/article.js
@@ -3,12 +3,15 @@ const deleteButton = document.getElementById("delete-btn");
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById("article-id").value;
-        fetch(`/api/articles/${id}`, {method: 'DELETE'})
-            .then(() => {
-                alert('Success to delete');
-                location.replace("/articles");
-                }
-            )
+        function success() {
+            alert('Successfully deleted');
+            location.replace('/articles');
+        }
+        function fail() {
+            alert('fail in deleting');
+            location.replace('/articles');
+        }
+        httpRequest('DELETE', '/api/articles/' + id, null, success, fail)
     })
 }
 
@@ -21,15 +24,22 @@ if (modifyButton) {
         let title = document.getElementById("title").value;
         let content = document.getElementById("content").value;
 
-        fetch(`/api/articles/${id}`, {
-            method:'PUT',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({title: title, content: content})
+        body = JSON.stringify({
+            title: title,
+            content: content
         })
-            .then(() => {
-                alert("Modified");
-                location.replace(`/articles/${id}`);
-            });
+
+        function success() {
+            alert('Successfully modified');
+            location.replace('/articles/' + id);
+        }
+
+        function fail() {
+            alert('Modification failed');
+            location.replace('articles/' + id);
+        }
+
+        httpRequest('PUT', '/api/articles/'+ id, body, success, fail);
     });
 }
 
@@ -37,19 +47,73 @@ const createButton = document.getElementById('create-btn');
 
 if (createButton) {
     createButton.addEventListener('click', event => {
-        fetch('/api/articles',{
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
+            body = JSON.stringify({
                 title: document.getElementById('title').value,
                 content: document.getElementById('content').value
-            })
-        })
-            .then(() => {
-                alert("Created");
+            });
+            function success() {
+                alert('Successfully created');
                 location.replace('/articles');
-            })
+            }
+            function fail() {
+                alert('fail in creating');
+                location.replace('/articles');
+            }
+
+            httpRequest('POST', '/api/articles', body, success, fail)
     })
+}
+
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(';');
+    cookie.some(function (item) {
+        item = item.replace(" ", "");
+
+        var dic = item.split('=');
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+
+    return result;
+}
+
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: {
+
+            Authorization: "Bearer " + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json'
+        },
+        body: body
+    })
+        .then((response) => {
+            if (response.status === 201 || response.status === 200) {
+                return success();
+            }
+            const refreshToken = getCookie('refresh_token');
+            if (response.status === 401 && refreshToken) {
+                fetch ('/api/token', {
+                    method: 'POST',
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json'
+                    }
+                    body: JSON.stringify({
+                        refreshToken: getCookie('refresh_token')
+                    })
+                })
+                    .then((result) => {
+                        localStorage.setItem('access_token', result.accessToken);
+                        httpRequest(method, url, body, success, fail);
+                    })
+                    .catch((error) => fail());
+            } else {
+                return fail();
+            }
+        })
 }
