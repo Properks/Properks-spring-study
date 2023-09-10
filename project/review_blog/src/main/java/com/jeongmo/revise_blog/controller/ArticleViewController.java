a365c55fc33ecbbe582233e68a1a87c5ae088922
Jeongmo <wjdah1402@gmail.com>
2023-09-10 15:41:31 +0900

    :sparkles: Create new article page which can add and modify with thymeleaf

--- project/review_blog/src/main/java/com/jeongmo/revise_blog/controller/ArticleViewController.java
+++ project/review_blog/src/main/java/com/jeongmo/revise_blog/controller/ArticleViewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -28,9 +29,18 @@ public class ArticleViewController {
     @GetMapping("/article/{id}")
     public String viewArticle(Model model, @PathVariable Long id) {
         ArticleViewResponse response = new ArticleViewResponse(articleService.getArticle(id));
-        model.addAttribute("response", response);
+        model.addAttribute("viewArticle", response);
         return "mainPage";
     }
 
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id != null) {
+            model.addAttribute("newArticle", new ArticleViewResponse(articleService.getArticle(id)));
+        } else {
+            model.addAttribute("newArticle", new ArticleViewResponse());
+        }
+        return "mainPage";
+    }
 
 }
