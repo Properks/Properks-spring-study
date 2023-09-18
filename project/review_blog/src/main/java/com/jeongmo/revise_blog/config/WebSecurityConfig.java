812961a0b314d0d449470dd5029b9a6d2da10207
Jeongmo <wjdah1402@gmail.com>
2023-09-18 11:24:05 +0900

    :recycle: Remove @EnableWebMvc, @EnableWebSecurity and WebMvcConfigurer and add antMatcher("/js/**")

--- project/review_blog/src/main/java/com/jeongmo/revise_blog/config/WebSecurityConfig.java
+++ project/review_blog/src/main/java/com/jeongmo/revise_blog/config/WebSecurityConfig.java
@@ -7,60 +7,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
-@EnableWebMvc
 public class WebSecurityConfig {
 
     private static final String LOGIN = "/login";
     private final CustomUserDetailService service;
 
     @Bean
-    public WebSecurityCustomizer configure() {
+    WebSecurityCustomizer configure() {
         return (web -> web.ignoring()
                 .requestMatchers(toH2Console())
-                .requestMatchers(AntPathRequestMatcher.antMatcher("/css/**"))
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/css/**")) // No "/static/**",
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/img/**"))
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/js/**"))
         );
     }
 
-    @Bean
-    WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/css/**")
-                        .addResourceLocations("classpath:/static/css/")
-                        .setCachePeriod(0)
-                        .resourceChain(false);
-                registry.addResourceHandler("/js/**")
-                        .addResourceLocations("classpath:/static/js/")
-                        .setCachePeriod(0)
-                        .resourceChain(false);
-                registry.addResourceHandler("/img/**")
-                        .addResourceLocations("classpath:/static/img/")
-                        .setCachePeriod(0)
-                        .resourceChain(false);
-            }
-        };
-    }
+    // If i input @EnableWebMvc and didn't input WebMvcConfigurer(addResourceHandler), i can't get static resource
 
     @Bean
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector intro) {
@@ -90,11 +65,11 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder passwordEncoder) throws Exception {
         AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
 
         builder.userDetailsService(service)
-                .passwordEncoder(passwordEncoder());
+                .passwordEncoder(passwordEncoder);
 
         return builder.build();
 
