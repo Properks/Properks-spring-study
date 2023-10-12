c1b1016d3015454a74ad3f77c05de164262dd3b3
Jeongmo <wjdah1402@gmail.com>
2023-10-12 14:50:50 +0900

    :bulb: Add comment on method in Service folder.

--- project/review_blog/src/main/java/com/jeongmo/revise_blog/service/ArticleService.java
+++ project/review_blog/src/main/java/com/jeongmo/revise_blog/service/ArticleService.java
@@ -20,24 +20,46 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
-    // Method create an new article
+    /**
+     * Create article and save database
+     *
+     * @param request The information of article with CreateArticleRequest form
+     * @return The saved article with article form.
+     */
     public Article createArticle(@NotNull CreateArticleRequest request) {
         User author = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return articleRepository.save(request.toEntity(author));
     }
 
-    // Method find an article
+    /**
+     * Find and get an article.
+     *
+     * @param id The id of article you want to find
+     * @exception IllegalArgumentException When it can't find article
+     * @return The found article
+     */
     public Article getArticle(Long id) {
         return articleRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find Article"));
     }
 
-    // Method find all article
+    /**
+     * Find and get all articles
+     *
+     * @return The found articles with List form
+     */
     public List<Article> getArticles() {
         return articleRepository.findAll();
     }
 
-    // Method update article
+    /**
+     * Update article as new title and content
+     *
+     * @param id The id of article you will update
+     * @param request The UpdateArticleRequest to update
+     * @throws IllegalArgumentException If it can't find article
+     * @return The updated article
+     */
     @Transactional
     public Article updateArticle(Long id, UpdateArticleRequest request) {
         Article updatedArticle = articleRepository.findById(id)
@@ -46,7 +68,12 @@ public class ArticleService {
         return updatedArticle;
     }
 
-    // Method delete an article
+    /**
+     * Delete an article
+     *
+     * @param id The id to be deleted
+     * @throws IllegalArgumentException If it can't find article
+     */
     public void deleteArticle(Long id) {
         if (articleRepository.existsById(id)) {
             articleRepository.deleteById(id);
--- project/review_blog/src/main/java/com/jeongmo/revise_blog/service/UserService.java
+++ project/review_blog/src/main/java/com/jeongmo/revise_blog/service/UserService.java
@@ -13,6 +13,11 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * Save user with password encoding
+     *
+     * @param dto The information of user will add
+     */
     public void save(AddUserRequest dto) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         userRepository.save(User.builder()
@@ -22,15 +27,34 @@ public class UserService {
                 .build());
     }
 
+    /**
+     * Find user with Email
+     *
+     * @param email The email to find user
+     * @throws IllegalArgumentException If it can't find user by email
+     * @return The found user
+     */
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(() ->
                 new IllegalArgumentException("Not Found " + email));
     }
 
+    /**
+     * Check email is already exist
+     *
+     * @param email The email to check redundancy
+     * @return boolean, true: The email is already exist
+     */
     public boolean isDuplicatedEmail(String email) {
         return userRepository.existsByEmail(email);
     }
 
+    /**
+     * Add random code behind the nickname
+     *
+     * @param nickname nickname which doesn't have code (nickname)
+     * @return nickname which have code (nickname#0000 form)
+     */
     private String setNicknameWithCode(String nickname) {
         int code = nickname.hashCode() % 10000;
         String newNickname = nickname + "#" + String.format("%04d", code);
@@ -41,6 +65,12 @@ public class UserService {
         return newNickname;
     }
 
+    /**
+     * Check whether nickname which have code is already exist
+     *
+     * @param nickname The nickname which have code to check
+     * @return boolean, true: The nickname is already exist
+     */
     private boolean isDuplicatedNickname(String nickname) {
         return userRepository.existsByNickname(nickname);
     }
