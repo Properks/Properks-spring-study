effc7c56d2af2bc2cfdd6fb4527caf47ca5b47f7
Jeongmo Seo <wjdah1402@gmail.com>
2024-01-02 15:00:25 +0900

    :zap: Chain fronted and backend about changing password

--- project/review_blog/src/main/resources/static/js/information.js
+++ project/review_blog/src/main/resources/static/js/information.js
@@ -1,3 +1,4 @@
+//Change nickname
 const changeNickname = document.getElementById("information-change-nickname-btn");
 
 if (changeNickname) {
@@ -31,6 +32,48 @@ if (changeNickname) {
                 }
                 else {
                     alert("Unknown error (" + response.status + ")")
+                }
+            })
+    })
+}
+
+//Change password
+const changePassword = document.getElementById('information-change-password-btn');
+
+if (changePassword) {
+    changePassword.addEventListener('click', () => {
+        let userId = document.getElementById("user-id").value;
+        let oldPassword = document.getElementById('information-current-password').value;
+        let newPassword = document.getElementById('information-new-password').value;
+        let checkNewPassword = document.getElementById('information-check-new-password').value;
+        let errorMessage = document.getElementById('information-change-password-error-message');
+        errorMessage.style.color = "crimson";
+        if (newPassword !== checkNewPassword) {
+            errorMessage.textContent = "Check New Password is different from New Password";
+            return;
+        }
+        fetch('/user/password', {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: userId,
+                oldPassword: oldPassword,
+                newPassword: newPassword
+            })
+        })
+            .then(response => {
+                if (response.status === 409) {
+                    errorMessage.textContent = "Current Password is not correct";
+                }
+                else if (response.status === 200) {
+                    errorMessage.textContent = "";
+                    alert("Change Password Successfully");
+
+                }
+                else {
+                    alert("Unknown error (" + response.status + ")");
                 }
             })
     })
