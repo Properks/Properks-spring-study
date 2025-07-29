5c6dc10884dc7a5cb5b671080a03315c0f9945a2
Jeongmo Seo <wjdah1402@gmail.com>
2025-07-29 16:01:30 +0900

    :rocket: chore: 스프링 내의 추상화 설명을 위한 example/springAbstraction 프로젝트 초기 세팅

---
+++ example/spring-abstraction/src/main/java/org/jeongmo/springabstraction/SpringAbstractionApplication.java
@@ -0,0 +1,13 @@
+package org.jeongmo.springabstraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAbstractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAbstractionApplication.class, args);
+	}
+
+}
---
+++ example/spring-abstraction/src/test/java/org/jeongmo/springabstraction/SpringAbstractionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeongmo.springabstraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAbstractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
