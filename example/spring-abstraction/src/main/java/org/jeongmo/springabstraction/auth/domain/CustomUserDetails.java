dcf1f435db525cd58eb345f1661973866b22b5b6
Jeongmo Seo <wjdah1402@gmail.com>
2025-07-30 15:52:40 +0900

    :sparkles: feat: JwtFilter 초기 코드 작성

---
+++ example/spring-abstraction/src/main/java/org/jeongmo/springabstraction/auth/domain/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package org.jeongmo.springabstraction.auth.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.jeongmo.springabstraction.domain.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+}
---
+++ example/spring-abstraction/src/main/java/org/jeongmo/springabstraction/auth/filter/JwtFilter.java
@@ -0,0 +1,60 @@
+package org.jeongmo.springabstraction.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jeongmo.springabstraction.auth.domain.CustomUserDetails;
+import org.jeongmo.springabstraction.auth.token.JwtUtil;
+import org.jeongmo.springabstraction.domain.member.entity.Member;
+import org.jeongmo.springabstraction.domain.member.repository.MemberRepository;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(AUTHORIZATION_HEADER);
+        if (header != null && header.startsWith(TOKEN_PREFIX)) {
+            String token = header.substring(TOKEN_PREFIX.length());
+            try {
+                Long userId = jwtUtil.getUserId(token);
+                Member member = memberRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("사용자를 찾지 못했습니다."));
+
+                UserDetails userDetails = new CustomUserDetails(member);
+                Authentication authentication = UsernamePasswordAuthenticationToken.authenticated(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                filterChain.doFilter(request, response);
+            } catch (Exception e) {
+                handleException(response, e);
+            }
+        }
+        else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        response.setStatus(400);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getOutputStream(), "인증 실패했습니다.");
+    }
+}
